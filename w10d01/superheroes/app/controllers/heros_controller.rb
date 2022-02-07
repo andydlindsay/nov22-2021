d9f8c8fb74005aa0dca99530bc156e9ad3932774
Andy Lindsay <andydlindsay@gmail.com>
2022-02-07 10:19:01 -0800

    setup for lecture

--- w10d01/superheroes/app/controllers/heros_controller.rb
+++ w10d01/superheroes/app/controllers/heros_controller.rb
@@ -2,5 +2,7 @@ class HerosController < ApplicationController
   def index
     @power = Power.find params[:power_id]
     @heros = @power.heros
+
+    # render json: { power: @power, heros: @heros }
   end
 end
